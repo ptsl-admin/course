306602bd596b75608ad138968b24b9ae015f7fd0
Govinda Sapkota <60061411+ptsl-admin@users.noreply.github.com>
2023-03-17 15:41:58 +0000

    Starter working directory, Bootstrap files created

---
+++ src/main/java/com/course/example/course/Student.java
@@ -0,0 +1,50 @@
+package com.course.example.course;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student {
+    
+    // fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String name;
+
+    // empty constructor
+    public Student(){
+
+    }
+
+    // constructor
+    public Student(String name){
+        // no need to pass id, becasue the value is auto generated
+        this.name = name;
+    }
+
+    /* getters for fields */
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Student[id=%d,name='%s']", id,name);
+    }
+}
